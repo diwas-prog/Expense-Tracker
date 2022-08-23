21c2eddf72bb3a7c75d67c5555ea97cd6998e7f0
diwas-prog <diwaskapoor@gmail.com>
2022-08-23 17:18:34 +0530

    Updates

---
+++ precache-manifest.f246fd4ceb7a47f6fd51a8bd990293a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd55077dc0950dc906dfc5883bf717b",
+    "url": "/Expense-Tracker/index.html"
+  },
+  {
+    "revision": "199f097e23a9837c76a7",
+    "url": "/Expense-Tracker/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/Expense-Tracker/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Expense-Tracker/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "199f097e23a9837c76a7",
+    "url": "/Expense-Tracker/static/js/main.9294272d.chunk.js"
+  },
+  {
+    "revision": "242ecf752266ee6ab889",
+    "url": "/Expense-Tracker/static/js/runtime-main.c08c79a1.js"
+  }
+]);
